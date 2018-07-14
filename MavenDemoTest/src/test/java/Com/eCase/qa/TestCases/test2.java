7fc139822c674e0ce6cd7873db8bd0f69e354490
SS <SS@192.168.1.103>
2018-07-14 16:56:28 +0530

    a1233

--- MavenDemoTest/src/test/java/Com/eCase/qa/TestCases/test2.java
+++ MavenDemoTest/src/test/java/Com/eCase/qa/TestCases/test2.java
@@ -3,7 +3,7 @@ package Com.eCase.qa.TestCases;
 public class test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubdfdf
 System.out.println("Hello");
 	}
 
